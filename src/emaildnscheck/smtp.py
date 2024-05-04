d65e5761fb43d52c644c9876b910185046e23c00
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2024-05-03 21:44:27 -0400

    refactor: code cleanup for smtp.py

--- src/emaildnscheck/smtp.py
+++ src/emaildnscheck/smtp.py
@@ -23,21 +23,6 @@ from checkdmarc.utils import (
 from checkdmarc.mta_sts import mx_in_mta_sts_patterns
 from checkdmarc.dnssec import test_dnssec, get_tlsa_records
 
-"""Copyright 2019-2023 Sean Whalen
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License."""
-
-
 TLS_CACHE = ExpiringDict(max_len=200000, max_age_seconds=1800)
 STARTTLS_CACHE = ExpiringDict(max_len=200000, max_age_seconds=1800)
 
