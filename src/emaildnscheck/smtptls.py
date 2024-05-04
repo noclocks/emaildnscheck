f1d3a3e46e82e31077d0bf36eea368827dd2479b
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2024-05-03 21:44:48 -0400

    refactor: code cleanup for smtptls.py

--- src/emaildnscheck/smtptls.py
+++ src/emaildnscheck/smtptls.py
@@ -13,20 +13,6 @@ from .constants import SYNTAX_ERROR_MARKER
 from .regex import WSP_REGEX, MAILTO_REGEX_STRING, HTTPS_REGEX
 from .utils import query_dns
 
-"""Copyright 2019-2023 Sean Whalen
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License."""
-
 SMTPTLSREPORTING_VERSION_REGEX_STRING = (
     rf"v{WSP_REGEX}*=" rf"{WSP_REGEX}*TLSRPTv1{WSP_REGEX}*;"
 )
